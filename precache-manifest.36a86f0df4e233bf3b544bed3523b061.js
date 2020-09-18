20f975fd48e841c51ca98cbb0ab53d7d25abd2a5
Vlad <freshmen17@ukr.net>
2020-09-18 16:51:12 +0300

    Updates

---
+++ precache-manifest.36a86f0df4e233bf3b544bed3523b061.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed73819f8702de6951b0da8aef7a638",
+    "url": "/ether/index.html"
+  },
+  {
+    "revision": "5e0f5fce8ee77aab794d",
+    "url": "/ether/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "d19e232b2c0267788503",
+    "url": "/ether/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e0f5fce8ee77aab794d",
+    "url": "/ether/static/js/2.455fd1f4.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/ether/static/js/2.455fd1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d19e232b2c0267788503",
+    "url": "/ether/static/js/main.82a2e0e5.chunk.js"
+  },
+  {
+    "revision": "72050cac10de3693c108",
+    "url": "/ether/static/js/runtime-main.df452b01.js"
+  }
+]);
